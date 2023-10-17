55e95f4c5186a28af1463d7724df66ed079cf7f6
Marlie Mae Awayan <128160451+Marlie-Mae@users.noreply.github.com>
2023-10-17 17:37:01 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+package polymorphism;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Dog d = new Dog();
+		Cat c = new Cat();
+		
+		d.makeSound();
+		c.makeSound();
+	}
+
+}
